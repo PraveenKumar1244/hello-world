f7328635e8f5227c99993006c7e020ff4a146d20
praveenkumar.gouni <praveenkumar.gouni@accenture.com>
2017-10-14 14:16:58 +0530

    General testing

---
+++ General/src/NullCheck.java
@@ -0,0 +1,13 @@
+
+public class NullCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = null;
+		
+		System.out.println(Integer.parseInt(s));
+
+	}
+
+}
